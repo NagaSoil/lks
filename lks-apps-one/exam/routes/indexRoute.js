259a73dd4d23009ecaecc1f25d33e4daf4574bca
NagaSoil <106381208+NagaSoil@users.noreply.github.com>
2022-10-11 20:30:04 +0700

    update route indexroute.js

--- lks-apps-one/exam/routes/indexRoute.js
+++ lks-apps-one/exam/routes/indexRoute.js
@@ -2,16 +2,31 @@ const indexController  = require('../controllers/indexController')
 
 module.exports = (app) => {
     
-    app.route('/')
-        .get(indexController.index)
-
-    app.route('/apiv2/quiz')
-        .get(indexController.getAllData)
-
-    app.route('/init')
-        .get(indexController.create)
+    // app.route('/')
+    //     .get(indexController.index)
     
-    app.route('/flush')
+    // app.route('/quiz')
+    //     .get(indexController.getAllData)    
+        
+    // app.route('/init')
+    //     .get(indexController.create)
+
+    // app.route('/flush')
+    //     .get(indexController.flush)
+
+    app.route('/exam')
+        .get(indexController.index)
+    
+    app.route('/exam/quiz')
+        .get(indexController.getAllData)    
+        
+    app.route('/apiv2/quiz')
+        .get(indexController.getAllData)    
+    
+    app.route('/exam/init')
+        .get(indexController.create)
+
+    app.route('/exam/flush')
         .get(indexController.flush)
 
 }
